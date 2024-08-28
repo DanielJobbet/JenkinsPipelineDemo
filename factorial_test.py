bb59312a98d8cd7838d8f62d9baefdb794890d79
Daniel Thomas <daniel.thomas.jobbet@gmail.com>
2024-08-28 15:50:46 +0200

    Adding test files

---
+++ factorial_test.py
@@ -0,0 +1,19 @@
+import unittest
+import factorial
+
+
+class FactorialTestCase(unittest.TestCase):
+    def test_zero(self):
+        self.assertEqual(factorial.factorial(0), 1) , "Factorial of 0 should be 1!"
+
+    def test_some_numbers(self):
+        self.assertEqual(factorial.factorial(2), 2)
+        self.assertEqual(factorial.factorial(3), 6)
+        self.assertEqual(factorial.factorial(40), 24)
+
+    def test_some_other_numbers(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
