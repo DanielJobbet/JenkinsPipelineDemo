25122b199b6e6c5fbae5ac756f2c5460909d25af
Daniel Thomas <daniel.thomas.jobbet@gmail.com>
2024-08-29 23:09:51 +0200

    Added env-83

--- tests/factorial_test.py
+++ tests/factorial_test.py
@@ -7,9 +7,9 @@ class FactorialTestCase(unittest.TestCase):
         self.assertEqual(factorial(0), 1) , "Factorial of 0 should be 1!"
 
     def test_some_numbers(self):
-        self.assertEqual(factorial(2), 2)
-        self.assertEqual(factorial(3), 6)
-        self.assertEqual(factorial(40), 24)
+        self.assertEqual(factorial(2), 2), "The factorial of 2 is not equal to 2!"
+        self.assertEqual(factorial(3), 6), "The factorial of 3 is not equal to 6!"
+        self.assertEqual(factorial(40), 24), "The factorial of 40 is not equal to 24!"
 
 if __name__ == '__main__':
     unittest.main()
