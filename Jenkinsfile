e499ab929d63513e74612bc691bd5ffa089de76c
Daniel Thomas <daniel.thomas.jobbet@gmail.com>
2024-08-30 08:51:14 +0200

    Added env-122

--- Jenkinsfile
+++ Jenkinsfile
@@ -24,7 +24,7 @@ pipeline {
                     python -m pytest -v tests/factorial_test.py --junit-xml=results.xml
                     if %ERRORLEVEL% neq 0 exit 0
                 '''
-                junit skipMarkingBuildUnstable: true, allowEmptyResults: true, testResults: 'results.xml', skipPublishingChecks: true
+                junit allowEmptyResults: true, testResults: 'results.xml', skipPublishingChecks: true
             }
         }
     }
--- tests/factorial_test.py
+++ tests/factorial_test.py
@@ -9,7 +9,7 @@ class FactorialTestCase(unittest.TestCase):
     def test_some_numbers(self):
         self.assertEqual(factorial(2), 2), "The given factorial is not equal to 2!"
         self.assertEqual(factorial(3), 6), "The given factorial is not equal to 6!"
-        self.assertEqual(factorial(40), 24), "The given factorial is not equal to 24!"
+        self.assertEqual(factorial(4), 24), "The given factorial is not equal to 24!"
 
 if __name__ == '__main__':
     unittest.main()
